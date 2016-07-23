8e4f04c6a469a53afb826be8ccc62e7ffdb53465
Simon Lepel <simbo@simbo.de>
2016-07-23 14:10:45 +0200

    replace jade with pug; support all options supported by pug compiler; some refactoring; add editorconfig

--- index.js
+++ index.js
@@ -3,59 +3,65 @@
 	Author Scott Beck @bline
 */
 
-var loaderUtils = require("loader-utils");
-var Path = require('path');
+var loaderUtils = require('loader-utils');
+var path = require('path');
 
 module.exports = function(source) {
+
 	this.cacheable && this.cacheable(true);
-	var jade = require("jade");
-	var query = loaderUtils.parseQuery(this.query);
 
-	var dirname = Path.dirname(this.resourcePath);
+	var query = loaderUtils.parseQuery(this.query),
+		options = this.options,
+		pugConfig = options[query.configKey || 'pugHtml'] || {},
+		pug = pugConfig.pug || require('pug'),
+		pugOptions = {
+			filename: this.resourcePath,
+			compileDebug: query.compileDebug || pugConfig.compileDebug,
+			basedir: query.basedir || pugConfig.basedir,
+			pretty: query.pretty || pugConfig.pretty,
+			doctype: query.doctype || pugConfig.doctype,
+			inlineRuntimeFunctions: query.inlineRuntimeFunctions || pugConfig.inlineRuntimeFunctions,
+			globals: query.globals || pugConfig.globals,
+			self: query.self || pugConfig.self,
+			includeSources: query.compileDebug || pugConfig.compileDebug,
+			debug: query.debug || pugConfig.debug,
+			templateName: query.name  || pugConfig.name,
+			filters: query.filters || pugConfig.filters,
+			filterOptions: query.filterOptions || pugConfig.filterOptions,
+			plugins: query.plugins || pugConfig.plugins
+		},
+		template = pug.compileClientWithDependenciesTracked(source, pugOptions),
+		modOptions = Object.keys(options).reduce(function(acc, key) {
+			acc[key] = options[key];
+			return acc;
+		}, {}),
+		moduleBody, module, moduleRequire;
 
-	var tmpl = jade.compileClientWithDependenciesTracked(source, {
-		filename: this.resourcePath,
-		self: query.self,
-		pretty: query.pretty,
-		locals: query,
-		compileDebug: true,
-		externalRuntime: false
-	});
-
-	tmpl.dependencies.forEach(function(dep) {
+	template.dependencies.forEach(function(dep) {
 		this.addDependency(dep);
 	}.bind(this));
 
-	var opts = this.options;
-
-	var loaders = opts.module ? opts.module.loaders : opts.resolve.loaders;
-
-	var mopts = Object.keys(opts).reduce(function(acc, key) {
-		acc[key] = opts[key];
-		return acc;
-	}, {});
-
-	mopts.recursive = true;
-	mopts.resolve = {
-		loaders: loaders,
-		extensions: opts.resolve.extensions,
-		modulesDirectories: (opts.resolve.modulesDirectories || []).concat(opts.resolve.fallback || [])
+	modOptions.recursive = true;
+	modOptions.resolve = {
+		loaders: options.module ? options.module.loaders : options.resolve.loaders,
+		extensions: options.resolve.extensions,
+		modulesDirectories: (options.resolve.modulesDirectories || []).concat(options.resolve.fallback || [])
 	};
 
-	var er = 'var jade = require(' + resolve('jade/runtime') + ');\nrequire = require(' + resolve('enhanced-require') + ')(module, require(' + resolve('./json2regexp') + ')(' +
-				JSON.stringify(mopts, toString) + '));\n';
+	moduleBody = 'var pug=require(' + resolve('pug-runtime') + ');' +
+		'require=require(' + resolve('enhanced-require') + ')' +
+		'(module,require(' + resolve('./json2regexp') + ')' +
+		'(' + JSON.stringify(modOptions, toString) + '));' +
+		template.body + ';module.exports=template;template.__require=require';
+	module = this.exec(moduleBody, this.resource);
+	moduleRequire = module.__require;
 
-	var moduleBody = er + tmpl.body + '\n\nmodule.exports = template;\ntemplate.__require = require';
-
-	var mod = this.exec(moduleBody, this.resource);
-
-	var _require = mod.__require;
-
-	for (var file in _require.contentCache) {
+	for (var file in moduleRequire.contentCache) {
 		this.addDependency && this.addDependency(file);
 	}
 
-	return mod(query.locals || query);
+	return module(query.locals || query);
+
 }
 
 function resolve(path) {
